d82ee1f4c5a5e7d4d3d88d108053ed96c5b5078b
Urte Žarnauskaitė <urte.yar@gmail.com>
2020-09-30 16:15:57 +0300

    Data & img

--- js/data.js
+++ js/data.js
@@ -1,3 +1,37 @@
-const posts = [];
+/*
+autoriaus 
+    name
+    surname
+    avataras (profilio nuotrauka)
+    profilio nuoroda
+time
+post 
+    text
+    photo gallery
+    background
+*/
+
+const posts = []
+
+const post = {
+    author: {
+        name: 'Vardenis',
+        lastName: 'Surname',
+        avatar: './img/users/varpav.jpg',
+        uri: 'vardenis-pavardenis'
+    },
+    timestamp: 15313131588745,
+
+    content: {
+        text: 'Laba diena!',
+        images: [
+            '.img',
+            '.img2'
+        ],
+        background: 'red'
+    }
+}
 
 export default posts;
+
+
