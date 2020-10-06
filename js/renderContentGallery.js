59eee79eb97544343e95e6a9d5b03f49fd0da55d
Urte Žarnauskaitė <urte.yar@gmail.com>
2020-10-06 13:37:14 +0300

    update gallery js

--- js/renderContentGallery.js
+++ js/renderContentGallery.js
@@ -1,7 +1,29 @@
-function renderContentGallery (data) {
-    return `<div class="gallery">
-        IMAGES GALLERY
-        </div>`;
+function renderContentGallery(data) {
+    // validacija
+    if (!data){
+        console.warn('ERROR: generuojant galerija, reikia duoti tinkama informacija');
+        return '';
+    }
+    // logika
+
+    const maxGallerysize = 4;
+    const totalPhotosCount = data.length;
+    const visiblePhotoCount = totalPhotosCount > maxGallerysize ? maxGallerysize : totalPhotosCount;
+
+    let imagesHTML = '';
+    for (let i = 0; i < visiblePhotoCount; i++) {
+        imagesHTML += `<img src="./img/posts/${data[i]}" alt="User post gallery picture">`;
+    }
+
+    let extraHTML = '';
+    if (totalPhotosCount > maxGallerysize){
+        extraHTML = `<div class="extra">+${totalPhotosCount - maxGallerysize}</div>`;
+    }
+
+    return `<div class="gallery gallery-${visiblePhotoCount}">
+                ${imagesHTML}
+                ${extraHTML}
+            </div>`;
 }
 
 export default renderContentGallery;
