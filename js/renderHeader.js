f3772500bb917fdf2664fb300cab54390f948070
Urte Žarnauskaitė <urte.yar@gmail.com>
2020-10-08 20:39:19 +0300

    bug

--- js/renderHeader.js
+++ js/renderHeader.js
@@ -2,7 +2,7 @@ import formatTime from './formatTime.js';
 
 function renderHeader (data, time) {
     return `<header class="post-header">
-                <img class="avatar" src="./img/users/${data.photo}" alt="User profile picture"></img>
+                <img class="avatar" src="./img/users/${data.photo}" alt="User profile picture">
                 <div class="author">
                     <a class="user-link" href="#/user/${data.link}">${data.name} ${data.lastname}</a>
                     <div class="timestamp">
